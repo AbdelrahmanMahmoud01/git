fbaad4da8419f724371786516fd189905008ee94
AbdelrahmanMahmoud01 <aapiedraak@gmail.com>
2022-05-10 13:55:24 +0200

    default

---
+++ Problem Solving/Problem Solving/StudentAndNumbers/AbdoStudentAndNumbers.cs
@@ -0,0 +1,5 @@
+﻿namespace Problem_Solving.StudentAndNumbers;
+public static class AbdoStudentAndNumbers
+{
+    public static string CompareNumbers (int numOne,int numTwo) => numOne > numTwo? "First" :"Second";
+}
