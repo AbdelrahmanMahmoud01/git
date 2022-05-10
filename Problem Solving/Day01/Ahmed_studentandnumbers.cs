6e941d0b967ff0f15f325f643560d1ba4da9ef74
Ahmed Ibrahim <ebrahema89859@gmail.com>
2022-05-10 14:17:53 +0200

    Ahmed Day 01

---
+++ Problem Solving/Day01/Ahmed_studentandnumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Solving.Day01
+{
+    public static class Ahmed_studentandnumbers
+    {
+        public static string studentandnumber(long Num01, long Num02)
+        {
+            if (Num01 < Num02)
+            {
+                return ("first");
+            }
+            else if (Num01 > Num02)
+            {
+                return ("second");
+            }
+            else
+            {
+                return ("Equal");
+            }
+        }
+    }
+}
