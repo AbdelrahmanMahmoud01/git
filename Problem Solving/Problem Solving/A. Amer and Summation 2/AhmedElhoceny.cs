a537b7c352152d64ec1583bf1dd84241cee194f3
AhmedElhoceny <72575988+AhmedElhoceny@users.noreply.github.com>
2022-05-15 21:24:06 +0200

    A. Amer and Summation 2

---
+++ Problem Solving/Problem Solving/A. Amer and Summation 2/AhmedElhoceny.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Solving.A._Amer_and_Summation_2
+{
+    class AhmedElhoceny
+    {
+        void A._Amer_and_Summation_2()
+        {
+            var Character = Console.ReadLine();
+            var ArrayData = Character.Split(" ");
+            var FirstStep = double.Parse(ArrayData[0]);
+            var LastStep = double.Parse(ArrayData[1]);
+            double FinalResult = 0;
+
+            for (double i = FirstStep; i <= LastStep; i++)
+            {
+                FinalResult += i;
+            }
+
+            Console.WriteLine(FinalResult);
+        }
+    }
+}
